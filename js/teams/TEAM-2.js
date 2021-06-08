34a4617127a04da84b7b3e67bdc44781e08d153f
fannyjohaaansson <70637201+fannyjohaaansson@users.noreply.github.com>
2021-06-08 17:26:14 +0200

    Update Team-2

--- js/teams/TEAM-2.js
+++ js/teams/TEAM-2.js
@@ -1,6 +1,6 @@
 const WORLD = {
-	title: 'team 2',
-	order: null, // [0,1,2]
+	title: 'Escaping HI-22',
+	order: [0,1,2,3,4,5]
 	levels: [
 		{
 			title: 'level 1',
@@ -12,6 +12,18 @@ const WORLD = {
 		},
 		{
 			title: 'level 3',
+			url: 'https://escapefromhyperisland.github.io/game/sample-world/level-3',
+		},
+		{
+			title: 'The cabbin',
+			url: 'https://fannyjohaaansson.github.io/escape-room/',
+		},
+		{
+			title: 'level 5',
+			url: 'https://escapefromhyperisland.github.io/game/sample-world/level-3',
+		},
+		{
+			title: 'level 6',
 			url: 'https://escapefromhyperisland.github.io/game/sample-world/level-3',
 		},
 	],
