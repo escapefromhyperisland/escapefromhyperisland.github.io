869451b6cb70735e885be15300b181cbd75261de
Rafe <rafenasir@gmail.com>
2021-06-09 00:44:20 +0200

    updated Team-2

--- js/teams/TEAM-2.js
+++ js/teams/TEAM-2.js
@@ -1,32 +1,32 @@
 const WORLD = {
-	title: 'Escaping HI-22',
-	order: [0,1,2,3,4,5],
-	levels: [
-		{
-			title: 'Hack the Vault',
-			url: 'https://space-game-level01.netlify.app/',
-		},
-		{
-			title: 'Star Bores',
-			url: 'https://adam-space-game.netlify.app/',
-		},
-		{
-			title: 'Foggy Forest',
-			url: ' https://ebbaj.github.io/a-frame-foggy-forest/',
-		},
-		{
-			title: 'The cabbin',
-			url: 'https://fannyjohaaansson.github.io/escape-room/',
-		},
-		{
-			title: 'Itsy Bitsy Space',
-			url: 'https://bitsy.frennert.com/',
-		},
-		{
-			title: 'level 6',
-			url: 'https://escapefromhyperisland.github.io/game/sample-world/level-3',
-		},
-	],
+  title: "Escaping HI-22",
+  order: [0, 1, 2, 3, 4, 5],
+  levels: [
+    {
+      title: "Hack the Vault",
+      url: "https://space-game-level01.netlify.app/",
+    },
+    {
+      title: "Star Bores",
+      url: "https://adam-space-game.netlify.app/",
+    },
+    {
+      title: "Foggy Forest",
+      url: " https://ebbaj.github.io/a-frame-foggy-forest/",
+    },
+    {
+      title: "The cabbin",
+      url: "https://fannyjohaaansson.github.io/escape-room/",
+    },
+    {
+      title: "Itsy Bitsy Space",
+      url: "https://bitsy.frennert.com/",
+    },
+    {
+      title: "Fly Back",
+      url: "https://hi-22.herokuapp.com/",
+    },
+  ],
 };
 
 export { WORLD };
