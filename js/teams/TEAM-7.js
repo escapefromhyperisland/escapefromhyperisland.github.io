95da643d1e488a659ae1d906a4cfdc511c89d8c2
Jonas Johansson <jns.johansson@gmail.com>
2021-06-08 10:47:11 +0200

    remove completed and local url

--- js/teams/TEAM-7.js
+++ js/teams/TEAM-7.js
@@ -1,25 +1,18 @@
 const WORLD = {
 	title: 'team 7',
-	startPage: null,
-	order: null,
+	order: null, // [0,1,2]
 	levels: [
 		{
 			title: 'level 1',
-			completed: false,
 			url: 'https://escapefromhyperisland.github.io/sample-world/level-1',
-			local_url: 'http://localhost:8080/sample-world/level-1',
 		},
 		{
 			title: 'level 2',
-			completed: false,
 			url: 'https://escapefromhyperisland.github.io/sample-world/level-2',
-			local_url: 'http://localhost:8080/sample-world/level-2',
 		},
 		{
 			title: 'level 3',
-			completed: false,
 			url: 'https://escapefromhyperisland.github.io/sample-world/level-3',
-			local_url: 'http://localhost:8080/sample-world/level-3',
 		},
 	],
 };
