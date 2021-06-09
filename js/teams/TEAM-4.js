6b13f0f9cc27252917c3cee80b71e5a567a845b0
Jonas Johansson <jns.johansson@gmail.com>
2021-06-09 15:30:30 +0200

    Update TEAM-4.js

--- js/teams/TEAM-4.js
+++ js/teams/TEAM-4.js
@@ -1,17 +1,9 @@
 const WORLD = {
 	title: 'team 4',
-	order: null, // [0,1,2]
+	order: [0,1],
 	credits: '',
 	description: '',
 	levels: [
-		{
-			title: 'level 1',
-			url: 'https://escapefromhyperisland.github.io/game/sample-world/level-1',
-		},
-		{
-			title: 'level 2',
-			url: 'https://escapefromhyperisland.github.io/game/sample-world/level-2',
-		},
 		{
 			title: 'level 3',
 			url: 'https://emmy-codes.github.io/puzzle-game/',
