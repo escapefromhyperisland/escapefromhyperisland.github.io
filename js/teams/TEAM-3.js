7b30a8eb4caad930442fcf7fa2aeecc160181ef1
Sumana Sattar <50830293+SumanaSattar@users.noreply.github.com>
2021-06-09 14:03:10 +0200

    Update TEAM-3.js

--- js/teams/TEAM-3.js
+++ js/teams/TEAM-3.js
@@ -17,8 +17,16 @@ const WORLD = {
 			url: 'https://mithila91.github.io/Mordor/',
 		},
 		{
+			title: 'hopis-nighmare ',
+			url: 'https://hyperhopi.github.io/hopis-nightmare/',
+		},
+		{
 			title: 'monsters under the bed ',
 			url: 'https://sumanasattar.github.io/A-frame-project/',
+		},
+		{
+			title: 'candy land ',
+			url: 'https://www.supipi.nu/candy-world/',
 		},
 		
 		
