68e1493ebf0466dcaaade0131b6bda89922d7d20
Lovisa Elensky <70098425+lovisaelensky@users.noreply.github.com>
2021-06-10 17:57:41 +0200

    Update TEAM-4.js

--- js/teams/TEAM-4.js
+++ js/teams/TEAM-4.js
@@ -1,6 +1,14 @@
 const WORLD = {
 	title: 'Team 4',
 	levels: [
+				{
+			title: 'Covid house',
+			url: 'https://escapefromhyperisland.github.io/world-4/level-1/',
+			author: {
+				name: 'Lovisa Elensky',
+				link: 'https://clever-ride-30d95b.netlify.app/',
+			},
+		},
 		{
 			title: 'The sleep demon',
 			url: 'https://emmy-codes.github.io/puzzle-game/',
