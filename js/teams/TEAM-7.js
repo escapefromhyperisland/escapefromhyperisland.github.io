44598de6beb8aafca797a8f374ce5b0fd135417e
Mishasergachev <70646059+Mishasergachev@users.noreply.github.com>
2021-06-10 14:55:26 +0200

    Update TEAM-7.js

--- js/teams/TEAM-7.js
+++ js/teams/TEAM-7.js
@@ -13,8 +13,8 @@ const WORLD = {
 			title: 'Cubesurfers',
 			url: 'https://escapefromhyperisland.github.io/Cubesurfers/',
 			author: {
-				name: '',
-				link: '',
+				name: 'Mikhail Sergachev',
+				link: 'https://github.com/Mishasergachev',
 			},
 		},
 	],
