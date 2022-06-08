f9bea53ee3e24a5b453d78c4abe32ac5d45447b2
Anna Sakurai <zenasak@gmail.com>
2022-06-08 10:41:27 +0200

    Update TEAM 3

--- js/fed23/TEAM-3.js
+++ js/fed23/TEAM-3.js
@@ -1,49 +1,49 @@
 const WORLD = {
-	title: 'Time-machine',
-	order: [0, 1, 2, 3, 4, 5],
-	title: 'Team 3',
-	levels: [
-		{
-			title: 'Steampunk time',
-			url: 'https://escapefromhyperisland.github.io/team3-time-machine/Iwana/',
-			author: {
-				name: 'Iwana Städeli',
-				link: 'https://www.linkedin.com/in/iwanast/',
-			},
-		},
-		{
-			title: 'Sophie',
-			url: 'https://escapefromhyperisland.github.io/team3-time-machine/sophie/',
-			author: {
-				name: '',
-				link: '',
-			},
-		},
-		{
-			title: 'Ian',
-			url: 'https://escapefromhyperisland.github.io/team3-time-machine/Ian/',
-			author: {
-				name: '',
-				link: '',
-			},
-		},
-		{
-			title: 'Hannes',
-			url: 'https://escapefromhyperisland.github.io/team3-time-machine/Hannes/',
-			author: {
-				name: '',
-				link: '',
-			},
-		},
-		{
-			title: 'Anna',
-			url: 'https://escapefromhyperisland.github.io/team3-time-machine/Anna/',
-			author: {
-				name: '',
-				link: '',
-			},
-		}
-	],
+  title: "Time-machine",
+  order: [0, 1, 2, 3, 4, 5],
+  title: "Team 3",
+  levels: [
+    {
+      title: "Steampunk time",
+      url: "https://escapefromhyperisland.github.io/team3-time-machine/Iwana/",
+      author: {
+        name: "Iwana Städeli",
+        link: "https://www.linkedin.com/in/iwanast/",
+      },
+    },
+    {
+      title: "Sophie",
+      url: "https://escapefromhyperisland.github.io/team3-time-machine/sophie/",
+      author: {
+        name: "",
+        link: "",
+      },
+    },
+    {
+      title: "Ian",
+      url: "https://escapefromhyperisland.github.io/team3-time-machine/Ian/",
+      author: {
+        name: "",
+        link: "",
+      },
+    },
+    {
+      title: "Hannes",
+      url: "https://escapefromhyperisland.github.io/team3-time-machine/Hannes/",
+      author: {
+        name: "",
+        link: "",
+      },
+    },
+    {
+      title: "Anna",
+      url: "https://escapefromhyperisland.github.io/team3-time-machine/Anna/",
+      author: {
+        name: "Anna Sakurai",
+        link: "https://annasakurai.netlify.app/",
+      },
+    },
+  ],
 };
 
 export { WORLD };
