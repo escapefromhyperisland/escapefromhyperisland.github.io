cbfaced120909885115c09ddfbdfbf2a35777953
Iwana Städeli <78117193+iwanast@users.noreply.github.com>
2022-06-08 10:22:41 +0200

    Update Team3

--- js/fed23/TEAM-3.js
+++ js/fed23/TEAM-3.js
@@ -1,14 +1,48 @@
 const WORLD = {
+	title: 'Time-machine',
+	order: [0, 1, 2, 3, 4, 5],
 	title: 'Team 3',
 	levels: [
 		{
-			title: 'Iwana',
+			title: 'Steampunk time',
 			url: 'https://escapefromhyperisland.github.io/team3-time-machine/Iwana/',
+			author: {
+				name: 'Iwana Städeli',
+				link: 'https://www.linkedin.com/in/iwanast/',
+			},
+		},
+		{
+			title: 'Sophie',
+			url: 'https://escapefromhyperisland.github.io/team3-time-machine/sophie/',
 			author: {
 				name: '',
 				link: '',
 			},
 		},
+		{
+			title: 'Ian',
+			url: 'https://escapefromhyperisland.github.io/team3-time-machine/Ian/',
+			author: {
+				name: '',
+				link: '',
+			},
+		},
+		{
+			title: 'Hannes',
+			url: 'https://escapefromhyperisland.github.io/team3-time-machine/Hannes/',
+			author: {
+				name: '',
+				link: '',
+			},
+		},
+		{
+			title: 'Anna',
+			url: 'https://escapefromhyperisland.github.io/team3-time-machine/Anna/',
+			author: {
+				name: '',
+				link: '',
+			},
+		}
 	],
 };
 
