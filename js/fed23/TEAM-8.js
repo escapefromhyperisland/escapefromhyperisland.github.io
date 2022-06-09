c656b35fe3fe3fba74bd2ab9e251c9fd1d598d06
Ben Ciantar <benjamin.ciantar@hyperisland.se>
2022-06-09 21:30:22 +0200

    Removed placeholder level objects

--- js/fed23/TEAM-8.js
+++ js/fed23/TEAM-8.js
@@ -1,37 +1,13 @@
 const WORLD = {
 	title: 'Lost in Time',
-	order: [0, 1, 2, 3],
+	order: [0],
 	levels: [
-		{
-			title: 'The Wake Up',
-			url: 'https://escapefromhyperisland.github.io/world-8/cece/',
-			author: {
-				name: 'Cecilia Hallerby',
-				link: 'https://cecilia-hallerby.web.app/',
-			},
-		},
-		{
-			title: 'The Family House',
-			url: 'https://escapefromhyperisland.github.io/world8-level2/',
-			author: {
-				name: 'Linnéa Gistrand',
-				link: 'https://linneagistrand.com/',
-			},
-		},
 		{
 			title: 'Medieval Mishaps',
 			url: 'https://escapefromhyperisland.github.io/fed23-world-8/ben/',
 			author: {
 				name: 'Ben Ciantar',
 				link: 'https://github.com/BenCiantar',
-			},
-		},
-		{
-			title: 'The Headquarters',
-			url: 'https://escapefromhyperisland.github.io/world-8/elin/',
-			author: {
-				name: 'Elin Nordin',
-				link: 'https://elinnordin.com/',
 			},
 		},
 	],
