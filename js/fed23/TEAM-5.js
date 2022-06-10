436bb11a3c6fbe583db7dcaf32f0bc461ce17c24
Jonas Johansson <jns.johansson@gmail.com>
2022-06-10 13:02:53 +0200

    Update TEAM-5.js

--- js/fed23/TEAM-5.js
+++ js/fed23/TEAM-5.js
@@ -1,6 +1,5 @@
 const WORLD = {
-	title: 'Escape from Pleasantville',
-	order: [0, 1, 2, 3, 4, 5, 6],
+	title: '',
 	levels: [
 		{
 			title: 'Escape from a loundry room',
@@ -8,54 +7,6 @@ const WORLD = {
 			author: {
 				name: 'Anastasiya Samoilenka',
 				link: 'https://github.com/AnastasiyaSamoilenkaHyper',
-			},
-		},
-		{
-			title: 'The Hotel',
-			url: 'https://escapefromhyperisland.github.io/pleasantville/level-1',
-			author: {
-				name: 'Sofia Darke',
-				link: 'https://github.com/sofiadarkeweb',
-			},
-		},
-		{
-			title: 'The Dream',
-			url: 'https://escapefromhyperisland.github.io/pleasantville/level-2',
-			author: {
-				name: 'Agnes Kalström',
-				link: 'https://www.agneskalstrom.com/',
-			},
-		},
-		{
-			title: 'The Old Lady',
-			url: 'https://escapefromhyperisland.github.io/pleasantville/level-3',
-			author: {
-				name: 'Elin Amrén',
-				link: 'https://github.com/elinamren',
-			},
-		},
-		{
-			title: 'The Maze',
-			url: 'https://escapefromhyperisland.github.io/pleasantville/level-4',
-			author: {
-				name: '',
-				link: '',
-			},
-		},
-		{
-			title: 'The Medium',
-			url: 'https://escapefromhyperisland.github.io/pleasantville/level-5',
-			author: {
-				name: 'Anna Roos',
-				link: 'https://github.com/AnnaRoos',
-			},
-		},
-		{
-			title: 'The End',
-			url: 'https://escapefromhyperisland.github.io/pleasantville/level-6',
-			author: {
-				name: 'Neha Hirve',
-				link: 'https://nehahirve.github.io/',
 			},
 		},
 	],
