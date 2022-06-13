4609d1a98d247d698d7045c8e2e977a0cfc7476d
SaniaNaz <90827790+sanianaz@users.noreply.github.com>
2022-06-13 11:52:43 +0200

    Update TEAM-5.js

--- js/fed23/TEAM-5.js
+++ js/fed23/TEAM-5.js
@@ -1,15 +1,41 @@
 const WORLD = {
-	title: '',
-	levels: [
-		{
-			title: 'Escape from a loundry room',
-			url: 'https://escapefromhyperisland.github.io/FED23-team-5/Anastasiya/index.html',
-			author: {
-				name: 'Anastasiya Samoilenka',
-				link: 'https://github.com/AnastasiyaSamoilenkaHyper',
-			},
-		},
-	],
+  title: "Chaos",
+  order: [0, 1, 2, 3, 4, 5],
+  title: "Team 1",
+  levels: [
+    {
+      title: "Escape from a loundry room",
+      url: "https://escapefromhyperisland.github.io/FED23-team-5/Anastasiya/index.html",
+      author: {
+        name: 'Anastasiya Samoilenka",
+        link: "https://github.com/AnastasiyaSamoilenkaHyper",
+      },
+    },
+    {
+      title: "Golden House",
+      url: "https://escapefromhyperisland.github.io/FED23-team-5/Sania/index.html",
+      author: {
+        name: "Sania Akbar Naz",
+        link: "https://github.com/sanianaz",
+      },
+    },
+     {
+      title: "",
+      url: "",
+      author: {
+        name: "Isac",
+        link: "",
+      },
+    },
+     {
+      title: "",
+      url: "",
+      author: {
+        name: "Alex",
+        link: "",
+      },
+    },
+  ],
 };
 
 export { WORLD };
