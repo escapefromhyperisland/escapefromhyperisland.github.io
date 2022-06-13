8a82ead33242c4c2bc66e478f524e32f9866445c
SaniaNaz <90827790+sanianaz@users.noreply.github.com>
2022-06-13 16:01:35 +0200

    Update TEAM-5.js

--- js/fed23/TEAM-5.js
+++ js/fed23/TEAM-5.js
@@ -2,27 +2,27 @@ const WORLD = {
   title: "Team5-23",
   levels: [
     {
-      title: “Escape from a loundry room”,
-      url: “https://escapefromhyperisland.github.io/FED23-team-5/Anastasiya/index.html”,
+      title: "Escape from a loundry room",
+      url: "https://escapefromhyperisland.github.io/FED23-team-5/Anastasiya/index.html",
       author: {
-        name: "Anastasiya Samoilenka”,
-        link: “https://github.com/AnastasiyaSamoilenkaHyper”,
+        name: "Anastasiya Samoilenka",
+        link: "https://github.com/AnastasiyaSamoilenkaHyper",
               },
     },
     {
-      title: “Golden House”,
-      url: “https://escapefromhyperisland.github.io/FED23-team-5/Sania/index.html”,
+      title: "Golden House",
+      url: "https://escapefromhyperisland.github.io/FED23-team-5/Sania/index.html",
       author: {
-        name: “Sania Akbar Naz”,
-        link: “https://github.com/sanianaz”,
+        name: "Sania Akbar Naz",
+        link: "https://github.com/sanianaz",
               },
     },
      {
-      title: “Dino Kombat”,
-      url: “https://escapefromhyperisland.github.io/FED23-team-5/Isac/index.html”,
+      title: "Dino Kombat",
+      url: "https://escapefromhyperisland.github.io/FED23-team-5/Isac/index.html",
       author: {
-        name: “Isac”,
-        link: “https://github.com/isacgw”,
+        name: "Isac",
+        link: "https://github.com/isacgw",
               },
     },
   ],
