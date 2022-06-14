e56d96b45dc684f0d6de9796a2f3194d3b1d3a2c
Ben Ciantar <benjamin.ciantar@hyperisland.se>
2022-06-14 17:00:30 +0200

    Added Felix's Game

--- js/fed23/TEAM-8.js
+++ js/fed23/TEAM-8.js
@@ -10,6 +10,14 @@ const WORLD = {
 				link: 'https://github.com/BenCiantar',
 			},
 		},
+				{
+			title: 'The Mad Scientist in Ville de Crap',
+			url: 'https://escapefromhyperisland.github.io/fed23-world-8/felix/',
+			author: {
+				name: 'Felix Odenhall',
+				link: 'https://github.com/felix-odenhall',
+			},
+		},
 	],
 };
 
