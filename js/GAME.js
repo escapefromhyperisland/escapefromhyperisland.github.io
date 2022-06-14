1fd140b1816c999d367e548d214f54851a03105f
Jonas Johansson <jns.johansson@gmail.com>
2022-06-14 11:41:52 +0200

    Update GAME.js

--- js/GAME.js
+++ js/GAME.js
@@ -3,6 +3,7 @@ import { WORLD as WORLD_2 } from './fed23/TEAM-2.js'
 import { WORLD as WORLD_3 } from './fed23/TEAM-3.js'
 import { WORLD as WORLD_4 } from './fed23/TEAM-4.js'
 import { WORLD as WORLD_5 } from './fed23/TEAM-5.js'
+import { WORLD as WORLD_6 } from './fed23/TEAM-5.js'
 import { WORLD as WORLD_7 } from './fed23/TEAM-7.js'
 import { WORLD as WORLD_8 } from './fed23/TEAM-8.js'
 import { WORLD as WORLD_9 } from './fed23/TEAM-9.js'
@@ -10,7 +11,7 @@ import { WORLD as WORLD_9 } from './fed23/TEAM-9.js'
 const GAME = {
 	title: 'Escape from Hyper Island 2',
 	order: 'random',
-	worlds: [WORLD_1, WORLD_2, WORLD_3, WORLD_4, WORLD_5, WORLD_7, WORLD_8, WORLD_9],
+	worlds: [WORLD_1, WORLD_2, WORLD_3, WORLD_4, WORLD_5, WORLD_6, WORLD_7, WORLD_8, WORLD_9],
 }
 
 export { GAME }
