10d2662f16edaf1500e6e49304a2822c3fab0892
Jonas Johansson <jns.johansson@gmail.com>
2022-06-14 11:46:26 +0200

    Update TEAM-7.js

--- js/fed23/TEAM-7.js
+++ js/fed23/TEAM-7.js
@@ -1,41 +1,15 @@
 const WORLD = {
-  title: "Beach",
-  order: [0, 1, 2, 3],
-  title: "Team 1",
-  levels: [
-    {
-      title: "Beach Mystery",
-      url: "https://escapefromhyperisland.github.io/team6-beach-games/game/",
-      author: {
-        name: "Julia Wos",
-        link: "https://juliawos-portfolio.netlify.app/",
-      },
-    },
-    {
-      title: "3D-CRAB-SMASH",
-      url: "https://escapefromhyperisland.github.io/team6-beach-games/RichiGame/",
-      author: {
-        name: "Richard Kennedy",
-        link: "https://www.linkedin.com/in/richard-kennedy-818863ba/",
-      },
-    },
-     {
-      title: "Escaping hyperisland",
-      url: "https://escapefromhyperisland.github.io/team6-beach-games/ola",
-      author: {
-        name: "Olabinjo Muyiwa",
-        link: "https://www.linkedin.com/in/muyiwa-olabinjo-01a657228/",
-      },
-    },
-     {
-      title: "Beach Game",
-      url: "https://escapefromhyperisland.github.io/team6-beach-games/egiiiiil",
-      author: {
-        name: "Egil Swenning Leyser",
-        link: "https://egilswenningleyser.dev/",
-      },
-    },
-  ],
+	title: 'Team 7',
+	levels: [
+		{
+			title: 'ESC-SHAPES',
+			url: 'https://escapefromhyperisland.github.io/dream/sanna/',
+			author: {
+				name: 'Sanna Vogel',
+				link: '',
+			},
+		},
+	],
 };
 
 export { WORLD };
