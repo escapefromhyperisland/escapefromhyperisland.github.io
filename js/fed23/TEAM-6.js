e90efc6693fedc7e006505ada6a97c352e12dfd5
Jonas Johansson <jns.johansson@gmail.com>
2022-06-14 11:41:30 +0200

    Create TEAM-6.js

---
+++ js/fed23/TEAM-6.js
@@ -0,0 +1,31 @@
+const WORLD = {
+  title: "Team5-23",
+  levels: [
+    {
+      title: "Escape from a loundry room",
+      url: "https://escapefromhyperisland.github.io/FED23-team-5/Anastasiya/index.html",
+      author: {
+        name: "Anastasiya Samoilenka",
+        link: "https://github.com/AnastasiyaSamoilenkaHyper",
+              },
+    },
+    {
+      title: "Golden House",
+      url: "https://escapefromhyperisland.github.io/FED23-team-5/Sania/index.html",
+      author: {
+        name: "Sania Akbar Naz",
+        link: "https://github.com/sanianaz",
+              },
+    },
+     {
+      title: "Dino Kombat",
+      url: "https://escapefromhyperisland.github.io/FED23-team-5/Isac/index.html",
+      author: {
+        name: "Isac",
+        link: "https://github.com/isacgw",
+              },
+    },
+  ],
+};
+
+export { WORLD };
